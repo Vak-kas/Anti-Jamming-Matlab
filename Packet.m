2ddbf485d91fbdd2b4e9fb10db249a3b8f231327
서민재 <hey_minj@naver.com>
2026-05-21 12:41:38 +0900

    Packet 생성, 사이즈 기능

--- Packet.m
+++ Packet.m
@@ -1,16 +1,45 @@
 classdef Packet
     properties
+        type
+        size
+
         srcId
         dstId
         payload
     end
 
     methods
-        
-        function obj = Packet(srcId, dstId, payload)
+
+        % ========== 생성자 ==========
+        function obj = Packet(type, srcId, dstId, payload)
+            obj.type = type;
+            obj.size = obj.calculatePacketSize();
             obj.srcId = srcId;
             obj.dstId = dstId;
             obj.payload = payload;
+        end
+
+
+        % ========== 패킷 사이즈 ==========
+        function size = calculatePacketSize(obj)
+            switch obj.type
+
+                case PacketType.DATA
+                    size = 1500;
+
+                case PacketType.ACK
+                    size = 64;
+
+                case PacketType.NACK
+                    size = 64;
+
+                case PacketType.CONTROL
+                    size = 128;
+
+                otherwise
+                    size = 0;
+
+            end
         end
 
     end
