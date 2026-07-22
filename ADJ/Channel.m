33135af6ef613aeb7ef05156bd213be00be75acd
서민재 <hey_minj@naver.com>
2026-07-22 16:22:15 +0900

    feat : 채널 패키지 추가

---
+++ ADJ/Channel.m
@@ -0,0 +1,39 @@
+classdef Channel < handle
+    properties
+        Id %채널 번호
+        Type %ChannelType
+
+        Packets %Packet Array
+    end
+
+    methods
+        % ========== 생성자 ========== %
+        function obj = Channel(id, type)
+            obj.Id = id;
+            obj.Type = type;
+            obj.Packets = Packet.empty;
+        end
+
+        % ========== 채널 초기화 ========== %
+        function clearPacket(obj)
+            obj.Packets = Packet.empty;
+        end
+
+        % ========== 채널 패킷 가져오기 ========== %
+        function packets = getPacket(obj)
+            packets = obj.Packets;
+        end
+            
+
+
+        % ========== 패킷 추가 ========== %
+        function addPacket(obj, packet)
+            obj.Packets(end+1) = packet;
+        end
+
+
+
+    end
+    
+
+end
