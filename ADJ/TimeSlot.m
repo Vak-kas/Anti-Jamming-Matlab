d109ce92af47321bae18ff3fa86a78fa9741e94c
서민재 <hey_minj@naver.com>
2026-07-23 17:35:26 +0900

    feat : DQN넣기 전까지 전체적인 DATA송신/HARQ 수신 기능

---
+++ ADJ/TimeSlot.m
@@ -0,0 +1,83 @@
+classdef TimeSlot
+    properties
+        UseAgent
+    end
+
+    methods
+        function obj = TimeSlot()
+            env;
+            obj.UseAgent  = useAgent;
+        end
+
+        
+        function result = run(obj, slotIndex, UTs, Satellites, apjNode, ServiceChannels, ControlChannels)
+            %% 1. 채널 초기화
+            obj.clearChannels(ServiceChannels, ControlChannels);
+
+
+            %% 2. UT의 랜덤 채널 선택
+            for utIndex = 1:numel(UTs)
+                if obj.UseAgent
+                    % TODO - DQN 적용 예정
+                    selectedChannel = UTs(utIndex).Agent.selectAction();
+                else
+                    selectedChannel = randi(numel(ServiceChannels));
+                end
+
+                UTs(utIndex).SelectedChannel = selectedChannel;
+            end
+
+
+            %% 3. Data 패킷 생성
+            dataPackets = cell(1, numel(UTs));
+            for utIndex = 1:numel(UTs)
+                selectedChannel = UTs(utIndex).SelectedChannel;
+                dataPackets{utIndex} = Packet(PacketType.DATA, NodeType.UT, utIndex, NodeType.Satellite, utIndex, ChannelType.Service, selectedChannel);
+            end
+
+
+            %% 4. Service Channel에 Data 패킷 추가
+            for utIndex = 1:numel(UTs)
+                selectedChannel = dataPackets{utIndex}.ChannelId;
+                ServiceChannels(selectedChannel).addPacket(dataPackets{utIndex});
+            end
+
+            
+            %% 5. APJ 동작
+            
+
+            %% 6. Satellite의 HARQ 수행
+            for satelliteIndex = 1:numel(Satellites)
+                Satellites(satelliteIndex).performHARQ(ServiceChannels, ControlChannels, UTs);
+            end
+
+            DebugHelper.printChannels(ServiceChannels, ControlChannels);
+
+
+            %% 7. UT & APJ의 결과 확인 DQN 학습
+            if obj.UseAgent
+                % TODO - DQN 적용 예정
+            else
+            end
+
+
+            %% 결과 반환
+            result = 0;
+        end
+
+
+
+
+        %% 채널 초기화
+        function clearChannels(obj, ServiceChannels, ControlChannels)
+            for channelIndex = 1:numel(ServiceChannels)
+                ServiceChannels(channelIndex).clearPacket();
+            end
+
+            for channelIndex = 1:numel(ControlChannels)
+                ControlChannels(channelIndex).clearPacket();
+            end
+
+        end
+    end
+end
